ca539b801dec420e3aeadc9354c6716063108103
Arrackisarookie <arrackisarookie@gmail.com>
2024-03-28 21:53:11 +0800

    [M] 尝试了广播、断开连接

--- canteen/main.py
+++ canteen/main.py
@@ -1,25 +1,55 @@
-import json
-
+from starlette.websockets import WebSocketDisconnect
 from fastapi import FastAPI, WebSocket
 
 app = FastAPI()
 
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: dict):
+        for connection in self.active_connections:
+            await connection.send_json(message)
+
+
+manager = ConnectionManager()
+
+
 @app.websocket("/ws/chat/{room_name}/{user_name}")
 async def websocket_endpoint(websocket: WebSocket, room_name: str, user_name: str):
-    await websocket.accept()
-    content = {
-        "user_id": user_name,
-        "message": f"Welcome, {user_name}! You have joined the room: {room_name}"
-    }
-    await websocket.send_text(json.dumps(content, ensure_ascii=False))
+    try:
+        await manager.connect(websocket)
+        content = {
+            "user_id": "System",
+            "message": f"Welcome, {user_name}! You have joined the room: {room_name}"
+        }
+        await websocket.send_json(content)
 
-    while True:
-        data = await websocket.receive_text()
-        # 广播消息给其他连接到同一房间的所有客户端
-        # 在实际应用中，你需要维护一个连接池或者使用类似Redis Pub/Sub的方式
-        # 这里为了简化只演示接收消息的部分逻辑
-        print(f"Message from {room_name}-{user_name}: {data}")
+        while True:
+            data = await websocket.receive_text()
+            content = {
+                "user_id": user_name,
+                "message": f"{data}"
+            }
+            await manager.broadcast(content)
+    except WebSocketDisconnect as e:
+        manager.disconnect(websocket)
+        content = {
+            "user_id": "System",
+            "message": f"Client #{user_name} left the chat"
+        }
+        await manager.broadcast(content)
 
 
 # 启动应用
