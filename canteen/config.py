6f4cefa810e97f41bdbf9df6c0d85ab4bb246c28
李呲花 <Arrackisarookie@gmail.com>
2024-03-29 15:06:37 +0800

    [A] 实现了加入、离开、欢迎

---
+++ canteen/config.py
@@ -0,0 +1,12 @@
+import json
+
+from canteen.model import Room
+
+with open("resource/rooms.json", "r", encoding="utf-8") as f:
+    raw_rooms = json.load(f)
+rooms: dict[str, Room] = {
+    room_id: Room(room_id) for room_id in raw_rooms
+}
+
+with open("resource/index.html", "r", encoding="utf-8") as f:
+    index_html = f.read()
--- canteen/main.py
+++ canteen/main.py
@@ -1,58 +1,43 @@
+from fastapi import FastAPI, WebSocket, WebSocketException
+from fastapi.responses import HTMLResponse
+from starlette import status
 from starlette.websockets import WebSocketDisconnect
-from fastapi import FastAPI, WebSocket
+
+from canteen.config import index_html, rooms
+from canteen.model import User, Message
+
 
 app = FastAPI()
 
 
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: list[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-    async def broadcast(self, message: dict):
-        for connection in self.active_connections:
-            await connection.send_json(message)
+@app.get("/")
+async def index():
+    return HTMLResponse(index_html)
 
 
-manager = ConnectionManager()
+@app.websocket("/{room_id}/{user_id}")
+async def websocket_endpoint(
+    websocket: WebSocket,
+    room_id: str,
+    user_id: str
+):
+    if room_id not in rooms:
+        raise WebSocketException(code=status.WS_1003_UNSUPPORTED_DATA)
+    room = rooms[room_id]
 
+    await room.connect_manager.connect(websocket)
+    user = User(user_id, websocket)
+    await room.welcome(user)
 
-@app.websocket("/ws/chat/{room_name}/{user_name}")
-async def websocket_endpoint(websocket: WebSocket, room_name: str, user_name: str):
     try:
-        await manager.connect(websocket)
-        content = {
-            "user_id": "System",
-            "message": f"Welcome, {user_name}! You have joined the room: {room_name}"
-        }
-        await websocket.send_json(content)
-
         while True:
-            data = await websocket.receive_text()
-            content = {
-                "user_id": user_name,
-                "message": f"{data}"
-            }
-            await manager.broadcast(content)
-    except WebSocketDisconnect as e:
-        manager.disconnect(websocket)
-        content = {
-            "user_id": "System",
-            "message": f"Client #{user_name} left the chat"
-        }
-        await manager.broadcast(content)
+            content = await websocket.receive_text()
+            message = Message(user, content)
+            await room.broadcast(message)
+    except WebSocketDisconnect:
+        await room.farewell(user)
 
 
-# 启动应用
 if __name__ == "__main__":
     import uvicorn
 
