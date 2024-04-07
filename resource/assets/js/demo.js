40c1adc32d2a081c9ea386287b2fc4ed0c33dee8
李呲花 <Arrackisarookie@gmail.com>
2024-04-07 10:07:51 +0800

    [M] 调整页面布局

--- resource/assets/js/demo.js
+++ resource/assets/js/demo.js
@@ -1,15 +1,15 @@
 var ws = null;
 
 ws = new WebSocket("ws://localhost:8000/room/main");
-ws.onmessage = function(event) {
+ws.onmessage = (event) => {
     var messages = document.getElementById('messages')
     var message = document.createElement('p')
     message.className = "mt-4"
     var content = document.createTextNode(event.data)
     message.appendChild(content)
     messages.appendChild(message)
+    messages.scrollTop = messages.scrollHeight;
 };
-event.preventDefault()
 
 function sendMessage(event) {
     var input = document.getElementById("messageText")
