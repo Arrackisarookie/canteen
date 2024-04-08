be8bfd1355e7a9c8dc35e6b0482350a975793800
李呲花 <Arrackisarookie@gmail.com>
2024-04-08 17:18:16 +0800

    [A] 增加跨域

--- canteen/main.py
+++ canteen/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
 
 from canteen.auth.router import router as auth_router
 from canteen.chat.router import router as chat_router
 
+origins = [
+    "http://localhost:8000",
+    "https://chat.aiar.site",
+    "https://canteen-three.vercel.app",
+]
+
 
 app = FastAPI()
 app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router, tags=["Auth"])
 app.include_router(chat_router, tags=["Chat"])
